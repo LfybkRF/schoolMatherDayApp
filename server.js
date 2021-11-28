14ac96e619675285ac50362a6dcdef607d2ecbb4
LfybkRF <56714206+LfybkRF@users.noreply.github.com>
2021-11-28 04:06:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.static(__dirname));
-app.use(express.static(path.resolve(__dirname, 'build')));
-
-app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
+// app.use(express.static(path.resolve(__dirname, 'build')));
+// 
+// app.get('*', (req,res) => {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'))
+// })
 
 
 app.listen(PORT)
